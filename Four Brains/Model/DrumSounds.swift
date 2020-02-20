4e81c6d61ff243b8e7f5f3d5da0763789153b9da
Marcus Kim <marcuskim1989@gmail.com>
2020-02-19 23:43:13 -0800

    correct drum sounds. load tuned drum audio files to AKMIDISampler. tuned files will stay restricted to certain MIDI note numbers. Previously, was using untuned drum sounds. This is causes  bleeding into other MIDI note numbers when add other MIDI note numbers. Always use tuned MIDI notes.

--- Four Brains/Model/DrumSounds.swift
+++ Four Brains/Model/DrumSounds.swift
@@ -14,20 +14,15 @@ class DrumSounds {
     let drums = AKMIDISampler()
     
     var currentBPM = 60
-    var drumSoundsToggleState = true
-    
-    //var output = AudioKit.output
-    
+    //var drumSoundsToggleState = true
     
     let sequencer = AKAppleSequencer(filename: "4tracks")
     init() {
         
-        let rideCymbalFile = try! AKAudioFile(readFileName: "rideCymbalSound.wav")
-        let snareDrumFile = try! AKAudioFile(readFileName: "snareDrumSound.wav")
-        let bassDrumFile = try! AKAudioFile(readFileName: "bassDrumSound.wav")
-        let hiHatFile = try! AKAudioFile(readFileName: "hiHatSound.mp3")
-        
-        drums.enableMIDI(1000)
+        let rideCymbalFile = try! AKAudioFile(readFileName: "open_hi_hat_A#1.wav")
+        let snareDrumFile = try! AKAudioFile(readFileName: "snare_D1.wav")
+        let bassDrumFile = try! AKAudioFile(readFileName: "bass_drum_C1.wav")
+        let hiHatFile = try! AKAudioFile(readFileName: "closed_hi_hat_F#1.wav")
         
         do{
         try drums.loadAudioFiles([rideCymbalFile,
@@ -39,28 +34,19 @@ class DrumSounds {
             print("error loading samples to drum object")
         }
         
+        //drums.enableMIDI(AudioKit.midi.client, name: "4tracks")
         
-        drums.volume = 1
         sequencer.clearRange(start: AKDuration(beats: 0), duration: AKDuration(beats: 100))
         sequencer.debug()
         sequencer.setGlobalMIDIOutput(drums.midiIn)
         sequencer.enableLooping(AKDuration(beats: 4))
         sequencer.setTempo(Double(currentBPM))
-        //AudioKit.output = drums
-        //output = drums
+       
         
     }
-    /*
-    func changeDrumsSoundsToggleState(){
-        if drumSoundsToggleState {
-            drumSoundsToggleState = false
-        } else {
-            drumSoundsToggleState = true
-        }
-    }
-    */
+   
     func playDrumSounds() {
-        /*
+        
         do {
             try AKSettings.setSession(category: .playAndRecord, with:  AVAudioSession.CategoryOptions.defaultToSpeaker)
                 
@@ -73,12 +59,16 @@ class DrumSounds {
         }catch {
             print("error in settings.setSession")
         }
-         */
-        sequencer.tracks[0].add(noteNumber: 60, velocity: 127, position: AKDuration(beats: 0), duration: AKDuration(beats: 1.0))
-        sequencer.tracks[0].add(noteNumber: 60, velocity: 127, position: AKDuration(beats: 1), duration: AKDuration(beats: 1.0))
-        sequencer.tracks[0].add(noteNumber: 60, velocity: 127, position: AKDuration(beats: 2), duration: AKDuration(beats: 1.0))
-        sequencer.tracks[0].add(noteNumber: 60, velocity: 127, position: AKDuration(beats: 3), duration: AKDuration(beats: 1.0))
-    
+         
+        sequencer.tracks[0].add(noteNumber: 34, velocity: 127, position: AKDuration(beats: 0), duration: AKDuration(beats: 1.0))
+        sequencer.tracks[0].add(noteNumber: 34, velocity: 127, position: AKDuration(beats: 0.5), duration: AKDuration(beats: 1.0))
+        sequencer.tracks[1].add(noteNumber: 26, velocity: 127, position: AKDuration(beats: 1), duration: AKDuration(beats: 1.0))
+        sequencer.tracks[1].add(noteNumber: 26, velocity: 127, position: AKDuration(beats: 1.5), duration: AKDuration(beats: 1.0))
+        sequencer.tracks[2].add(noteNumber: 24, velocity: 127, position: AKDuration(beats: 2), duration: AKDuration(beats: 1.0))
+        sequencer.tracks[2].add(noteNumber: 24, velocity: 127, position: AKDuration(beats: 2.5), duration: AKDuration(beats: 1.0))
+        sequencer.tracks[3].add(noteNumber: 30, velocity: 127, position: AKDuration(beats: 3), duration: AKDuration(beats: 1.0))
+        sequencer.tracks[3].add(noteNumber: 30, velocity: 127, position: AKDuration(beats: 3.5), duration: AKDuration(beats: 1.0))
+        
         if sequencer.isPlaying {
             sequencer.stop()
             sequencer.rewind()
